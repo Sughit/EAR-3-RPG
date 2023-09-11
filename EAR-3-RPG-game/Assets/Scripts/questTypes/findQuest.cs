cb3eb28e852da844c9c5fbbf3ef330be915e1188
Sughit <andreisorici2007@gmail.com>
2023-09-11 16:22:56 +0300

    quest lui Jack

---
+++ EAR-3-RPG-game/Assets/Scripts/questTypes/findQuest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class findQuest : MonoBehaviour
+{
+    public static bool axeCollected;
+    public Text textCollected;
+    public dialogManagerNPC quest;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            if(quest.questAccepted)
+            {
+                axeCollected=true;
+                textCollected.text = "Go and talk to Jack";
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(quest.questAccepted)
+        {
+            textCollected = GameObject.Find("Canvas/questWindow/questUINPC3(Clone)/text").GetComponent<Text>();
+        }
+        if(axeCollected)
+        {
+            quest.questCompleted = true;
+        }
+    }
+}
